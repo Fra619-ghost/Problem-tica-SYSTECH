54671e414078d7c89fe2554285125ae103815329
David Sánchez <164054263+Djave17@users.noreply.github.com>
2025-09-18 14:18:53 -0600

    Correccion de nombre de carpetas

--- Problematica_ESports/src/main/java/run/Main.java
+++ Problematica_ESports/src/main/java/run/Main.java
@@ -1,16 +1,15 @@
 package run;
 import models.*;
 import utils.ConsoleIO;
+import viewModel.AppVM;
+
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Sistema de Gestión de Torneos de eSports \n");
-
-        Categoria estrategia = new Categoria("Estrategia");
-        Juego clash = new Juego("Clash Royale", estrategia);
-        
-        Jugador j1 = new Jugador("Pedro Morales", "Halcón", 1500);
+        ConsoleIO io = new ConsoleIO();
+        AppVM app = new AppVM(io);
+        app.runMenuLoop();
     }
 
 }
